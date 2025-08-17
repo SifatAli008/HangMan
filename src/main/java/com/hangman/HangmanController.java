cf28c1206eb508a62233db05b3066f063f9a3a54
SifatAli008 <SifatAli008@gmail.com>
2025-08-17 09:09:37 +0600

    Fix keyboard layout: Arrange letters in alphabetical order (A-Z) and update styling to match design image

--- src/main/java/com/hangman/HangmanController.java
+++ src/main/java/com/hangman/HangmanController.java
@@ -59,9 +59,9 @@ public class HangmanController {
     
     private void setupKeyboard() {
         String[] letters = {
-            "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
-            "A", "S", "D", "F", "G", "H", "J", "K", "L",
-            "Z", "X", "C", "V", "B", "N", "M"
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z"
         };
         
         int col = 0;
